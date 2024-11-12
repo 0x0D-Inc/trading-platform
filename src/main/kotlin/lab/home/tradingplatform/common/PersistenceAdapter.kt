8e2fd6e5317ed2591b97c9681f56b5772d41daec
Jay <jaeho9929@gmail.com>
2024-11-12 20:20:50 +0900

    feat: add helper annotations

---
+++ src/main/kotlin/lab/home/tradingplatform/common/PersistenceAdapter.kt
@@ -0,0 +1,12 @@
+package lab.home.tradingplatform.common
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Component
+annotation class PersistenceAdapter(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+)
---
+++ src/main/kotlin/lab/home/tradingplatform/common/WebAdapter.kt
@@ -0,0 +1,13 @@
+package lab.home.tradingplatform.common
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+// @MustBeDocumented
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Component
+annotation class WebAdapter(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+)
